9a6eb116f810b4c6537a06f332359fd9b2f713ad
Óscar <martincordobes.omc@gmail.com>
2021-09-21 19:06:23 +0200

    ÓSCAR 21-09-2021 19:02 Tablas de multiplicar del 1 al 9

---
+++ TablasMultiplicar/src/paquete/Ejecuta.java
@@ -0,0 +1,18 @@
+package paquete;
+
+public class Ejecuta {
+
+	public static void main(String[] args) {
+		int resultado;
+		for(int i = 1; i < 10; i++) {
+			System.out.println("    TABLA DEL " + i);
+			for(int j = 1; j < 10; j++) {
+				resultado = i*j;
+				System.out.println(i + "   *   " + j + "   =   " + resultado);
+			}
+			System.out.println("-------------------");
+		}
+
+	}
+
+}
